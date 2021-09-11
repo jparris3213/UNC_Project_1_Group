bae3fb2f3d29bc3503841b4bf8d6183c0cffd5ce
Anthony Linebaugh <anthonylinebaugh95@gmail.com>
2021-09-11 12:34:15 -0400

    added a function that will get images through a API with nasa.gov

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+var requestUrl = "https://api.nasa.gov/planetary/apod?api_key=qsQaRTJvk3pICPh8Ta3EufSeNvUosCdNK2CVBlfm&count=4";
+var image1El = document.getElementById("image1")
+
+
+function getApiImages(){
+  fetch(requestUrl, {
+  method: 'GET',
+  credentials: 'same-origin',
+  redirect: 'follow',
+  })
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+
+  for(i=0;i<data.length;i++){
+    imageURL = data[i].url;
+    var alt = data[i].title;
+    var desc = data[i].explanation;
+    var h2El = document.createElement("h2");
+    var img = document.createElement("img");
+    var pEl = document.createElement("p");
+    h2El.innerText = alt;
+    pEl.innerText = desc;
+    img.src = imageURL;
+    img.setAttribute("alt", alt);
+    var src = image1El;
+    src.appendChild(h2El);
+    src.appendChild(img);
+    src.appendChild(pEl);
+    }
+  });
+}
+
+getApiImages();
