ae6363d6950d543ec1a958093a3834398734420a
Jacob Parris <typefortyproductions@gmail.com>
2021-09-19 22:14:30 -0400

    Merge Conflicts Resolved?

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,7 +63,7 @@ $("#search").click(function (event) {
             useCoordinates(lat, long);
             return zipCode;
         });
-    asteroidSection();
+    asteroidSection();//Needs to be set to only run the table creation the first time
     chickenLittle();
 });
 
@@ -94,8 +94,6 @@ var useCoordinates = function (lat, long) {
 //NASA Near Earth Object API
 function chickenLittle() {
     var today = moment().format('YYYY-MM-DD');
-    var todayhour = moment().format('YYYY-MM-DD HH:mm');
-    console.log(todayhour);
     var neoAPI = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + today + "&end_date=" + today + "&api_key=aU9gsLEa5EEkdwAiUCG77iWZ1guGb6eXR4VVR4rn";
     fetch(neoAPI, {
         method: 'GET',
@@ -109,19 +107,10 @@ function chickenLittle() {
             var neos = data['near_earth_objects'][today];
             console.log(neos);
             for (i = 0; i < table_length; i++) {
-<<<<<<< HEAD
-
-                
-                var asteroidName = neos[i].name;//Returns the Name of the NEO  
-=======
                 //Returns the Name of the NEO
                 var asteroidName = neos[i].name;  
->>>>>>> main
 
                 var closestpass = neos[i].close_approach_data[0].close_approach_date_full;
-                //var close_pass = neos[i].close_approach_data[0].close_approach_date_full;
-                //var days_remain = moclosestpass.diff(todayhour);
-                //console.log(days_remain + "Days Remain");
                 
 
                 var asteroidSize = neos[i].estimated_diameter.meters.estimated_diameter_max;
@@ -144,12 +133,9 @@ function chickenLittle() {
                 } else {
                     $("#asteroid_distruction_" + i).text("✌No threat");
                 };
-<<<<<<< HEAD
-                $("#date_clossest_" + i).text(closestpass);
+                $("#date_closest_" + i).text(closestpass);
 
 
-=======
->>>>>>> main
             };
         });
 };
@@ -257,11 +243,7 @@ function asteroidSection() {
 
         row.append("<td id='date_closest_" + i + "'>" +"closest" + "</td>");
 
-<<<<<<< HEAD
         //row.append("<td id='current_distance_" + i + "'>" +/*TODO*/"distance" + "</td>");
-=======
-        row.append("<td id='current_distance_" + i + "'>" +"distance" + "</td>");
->>>>>>> main
 
         tBodyAsteroid.append(row);
     };
@@ -454,13 +436,9 @@ function init() {
     searchAnimation(split_screen);
 };
 
-<<<<<<< HEAD
 init();
 
 $("#carouselExampleCaptions").on("click", function(){
     $("#info-table").attr("style","display:none");
     $("#carouselExampleCaptions").animate({ width: "100%" }, 1000);
 })
-=======
-init();
->>>>>>> main
